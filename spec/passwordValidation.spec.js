c5655a62525d279e516b209c84ffdf26ba0a0f0d
xolani <xolani.mgube@umuzi.org>
2019-12-09 10:53:32 +0200

    updated testing comments

--- spec/passwordValidation.spec.js
+++ spec/passwordValidation.spec.js
@@ -22,19 +22,19 @@ describe("passwordIsValid", () =>{
 
 //Tests for passwordIsOkay function
 describe("passwordIsOkay", () =>{
-    it("It should be true ", () =>{
+    it("Password is short and does not meet all the conditions ", () =>{
         expect(passwordIsOkay("xolani")).toBe("Password too short, less than 3 conditions are met");
     })
-    it("It should be true ", () =>{
+    it("3 conditions are met even though there are no capital letters ", () =>{
         expect(passwordIsOkay("xolani$100")).toBe("The Password is Okay");
     })
-    it("It should be true ", () =>{
+    it("all conditions are met", () =>{
         expect(passwordIsOkay("XOlaNi$100")).toBe("The Password is Okay");
     })
-    it("It should be true ", () =>{
+    it("3 conditions are met but password is short", () =>{
         expect(passwordIsOkay("Xol$100")).toBe("Password should have a minimum of 8 characters");
     })
-    it("It should be true ", () =>{
+    it("length is okay but other condiotions are not met", () =>{
         expect(passwordIsOkay("xolanixolani")).toBe("The password should meet atleast 3 conditions");
     })
 });
